100bdc14e2447192245be30f289f016bbe791508
PanLydka <kamillydka1@gmail.com>
2018-08-03 04:32:28 +0200

    start working on main component

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+export const MainComponent = () => {
+    return (
+        <div>
+            <div className="row center-xs header">
+                <h1 className="col-xs-6 header__title">
+                    formularz wprowadzania danych osobowych!
+            </h1>
+            </div>
+        </div>
+    )
+}
