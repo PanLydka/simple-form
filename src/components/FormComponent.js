edcafd50d48e83b86dfa797774ebf23dab589a60
PanLydka <kamillydka1@gmail.com>
2018-08-13 03:19:16 +0200

    remove flexboxgrid package, BoostrapCDN + Bootswatch, new structure css

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -33,7 +33,7 @@ export class FormComponent extends React.Component {
                 <div className="row center-xs header">
                     <h1 className="col-xs-6 header__title">
                         formularz wprowadzania danych osobowych!
-                </h1>
+                     </h1>
                 </div>
                 <div className="row center-xs form">
 
